7b54f7b301a8585cc1f383a11de826ec277736dd
z2160103 <z2160103@H1405884.intra.ctc-g.co.jp>
2017-08-28 15:21:55 +0900

    8/28

--- src/pg_ex21/Pg_ex21.java
+++ src/pg_ex21/Pg_ex21.java
@@ -3,42 +3,49 @@ package pg_ex21;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Pg_ex21 {
 
-	public static void main(String[] args) {
+	public static void pg_ex21() {
 
 		final String RECORD_FILE = "record.log";
 		final String INVOICE_FILE = "invoice.dat";
 
-		String file_path = "C:\\eclipse\\pleiades\\workspacepg_ex21_data";
+		String file_path = "C:\\eclipse\\pleiades\\workspace\\pg_ex21_data";
 
 		// record.log
 		int count=0;
 		User[] user = new User[10000];
-		//10000でいいか？
 
 		FileReader fr;
 		BufferedReader br;
 
 		try {
-			fr = new FileReader(file_path + RECORD_FILE);
+			fr = new FileReader(file_path + "\\" + RECORD_FILE);
 			br = new BufferedReader(fr);
 
+			FileWriter filewriter = new FileWriter(file_path + "\\" + INVOICE_FILE);
+
 			String line;
+			br.readLine();
 			while((line = br.readLine()) != null){
 				switch(line.charAt(0)){
 				case '1':
-					user[count] = new User(line.substring(2,14));
+					user[count] = new User(line.substring(2,15));
 					break;
-				case '9':
+				//case '9':
+				case '0':
 					//出力
-
+					filewriter.write(user[count].getOutput());
+					count++;
+					break;
 				default:
 					user[count].set(line);
 				}
 			}
+			filewriter.close();
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -46,8 +53,4 @@ public class Pg_ex21 {
 			e.printStackTrace();
 		}
 	}
-
-
-
-
 }
--- src/pg_ex21/TestPg_ex21.java
+++ src/pg_ex21/TestPg_ex21.java
@@ -1,28 +1,29 @@
 package pg_ex21;
 
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class TestPg_ex21 {
 
-
-	//テストから囲うと思ったけど、昨日が決まっていなくて掛けなかった
-
-
 	@Test
 	public void test() {
-		LoadFile.record();
+		Pg_ex21.pg_ex21();
+		//diff
+		Runtime runtime = Runtime.getRuntime();
+		Process p;
 
-
+		try {
+			p = runtime.exec("fc /B c:\\eclipse\\pleiades\\workspace\\pg_ex21_data\\invoice.dat_sample c:\\eclipse\\pleiades\\workspace\\pg_ex21_data\\invoice.dat");
+			assertEquals(0,p.waitFor());
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+		}catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
-
-
-
-
-
-
-
-
-
-	//invoice.dat
-
 }
