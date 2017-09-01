7ad79f7935982d795f45f5be0efc8a895530d6f4
z2160103 <z2160103@H1405884.intra.ctc-g.co.jp>
2017-09-01 09:38:12 +0900

    配列をArrayListにした

--- src/pg_ex21/Pg_ex21.java
+++ src/pg_ex21/Pg_ex21.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Pg_ex21 {
 
@@ -13,11 +14,12 @@ public class Pg_ex21 {
 		final String RECORD_FILE = "record.log";
 		final String INVOICE_FILE = "invoice.dat";
 
-		String file_path = "C:\\eclipse\\pleiades\\workspace\\pg_ex21_data";
+		String file_path = "C:/eclipse/pleiades/workspace/pg_ex21_data";
 
 		// record.log
 		int count=0;
-		User[] user = new User[10000];
+		//User[] user = new User[10000];
+		ArrayList<User> user = new ArrayList<User>();
 
 		FileReader fr;
 		BufferedReader br;
@@ -33,16 +35,19 @@ public class Pg_ex21 {
 			while((line = br.readLine()) != null){
 				switch(line.charAt(0)){
 				case '1':
-					user[count] = new User(line.substring(2,15));
+					//user[count] = new User(line.substring(2,15));
+					user.add(new User(line.substring(2,15)));
 					break;
 				//case '9':
 				case '0':
 					//出力
-					filewriter.write(user[count].getOutput());
+					//filewriter.write(user[count].getOutput());
+					filewriter.write(user.get(count).getOutput());
 					count++;
 					break;
 				default:
-					user[count].set(line);
+					//user[count].set(line);
+					user.get(count).set(line);
 				}
 			}
 			filewriter.close();
